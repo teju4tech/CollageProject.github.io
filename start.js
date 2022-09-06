b30f16507aa34cf7daaa101157583251e1d7230c
teju4tech <solankitehpalsingh59@gmail.com>
2022-09-06 22:21:10 +0530

    new project

---
+++ start.js
@@ -0,0 +1,17 @@
+function tab()
+    {
+        alert("wrong answer...")
+    }
+    function dabba()
+    {  
+        let a = 0;
+        a = a + 1;
+        if(a>1)
+        {
+            a = a+1;
+            document.getElementById("marks").innerHTML = "your marks is:"+a;
+        }
+        console.log(a);
+        document.getElementById("marks").innerHTML = "your marks is:"+a;
+        alert("right answer :)")
+    }
